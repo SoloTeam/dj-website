24fab82abd6630e8a3a75a3cc83356a555a4176d
Fedor Solovev <fedor.solovev@gmail.com>
2025-07-26 19:31:44 -0400

    Fix

--- terraform/startup-script.sh
+++ terraform/startup-script.sh
@@ -3,6 +3,7 @@ sudo apt-get update
 sudo apt-get install -y python3-pip git
 sudo pip3 install flask
 
+echo "Cloning repo..."
 mkdir -p /opt/dj-website
 cd /opt/dj-website
 
@@ -10,7 +11,11 @@ cd /opt/dj-website
 git clone https://github.com/SoloTeam/dj-website.git .
 
 cd app
+echo "Running Flask app..."
 # Modify app.py to bind to all interfaces on port 5000
 # sed -i 's/app.run(/app.run(host="0.0.0.0", /' app.py
 
 nohup python3 app.py > /var/log/flask.log 2>&1 &
+echo "===== STARTUP SCRIPT DONE ====="
+
+} >> /var/log/startup-script.log 2>&1
