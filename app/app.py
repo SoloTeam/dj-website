691e3533b4a5ced5ec7358370ef513291c70e63f
Fedor Solovev <fedor.solovev@gmail.com>
2025-07-26 21:58:25 -0400

    After reloader issue

--- app/app.py
+++ app/app.py
@@ -23,4 +23,5 @@ def home():
 
 # Run the app on localhost with debug mode
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0",debug=True,use_reloader=False)
