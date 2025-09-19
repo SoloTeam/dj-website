be14249f32615126b024504d097fee076625e18e
Fedor Solovev <fedor.solovev@gmail.com>
2025-09-19 09:00:53 -0400

    Commit

--- terraform/envs/prod/variables.tf
+++ terraform/envs/prod/variables.tf
@@ -17,3 +17,24 @@ variable "flask_port" {
   type        = number
   default     = 5003 # Default for FLASK, can be overwritten from .tfvars
 }
+
+/*
+# cloud-sql.tf (new file or append)
+resource "google_sql_database_instance" "dj-cloud-sql" {
+  name             = "dj-website-db"
+  database_version = "POSTGRES_15"
+  region           = var.region
+  settings { tier = "db-f1-micro" } # tiny; good for demo; resize later
+}
+
+resource "google_sql_database" "app" {
+  name     = "djdb"
+  instance = google_sql_database_instance.dj.name
+}
+
+resource "google_sql_user" "app" {
+  name     = "djuser"
+  instance = google_sql_database_instance.dj.name
+  password = var.db_password
+}
+*/
